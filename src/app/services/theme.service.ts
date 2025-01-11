ed950b57dd79145f518621c81bf474183fdf9656
Cicero Brunelli <cicerobrunelli@hotmail.com>
2025-01-11 12:32:38 -0300

    adiciona funcionalidade de alternância de modo escuro ao ThemeService

--- src/app/services/theme.service.ts
+++ src/app/services/theme.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ThemeService {
 
-  constructor() { }
+  private darkThemeClass = 'my-app-dark';
+  private themeKey = 'app-theme';
+
+  constructor() {
+    this.loadTheme();
+  }
+
+  toggleDarkMode(): boolean {
+    const element = document.querySelector('html');
+    if (element) {
+      element.classList.toggle(this.darkThemeClass);
+      const isDarkMode = element.classList.contains(this.darkThemeClass);
+      localStorage.setItem(this.themeKey, isDarkMode ? 'dark' : 'light');
+      return isDarkMode;
+    }
+    return false;
+  }
+
+  private loadTheme() {
+    const savedTheme = localStorage.getItem(this.themeKey);
+    if (savedTheme === 'dark') {
+      const element = document.querySelector('html');
+      if (element) {
+        element.classList.add(this.darkThemeClass);
+      }
+    }
+  }
 }
