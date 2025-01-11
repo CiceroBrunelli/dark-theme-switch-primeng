b7bd764ff37c38ba70c9f2b5c0de010b77557c94
Cicero Brunelli <cicerobrunelli@hotmail.com>
2025-01-11 12:37:17 -0300

    adiciona componente SwitchTheme para alternância de tema

---
+++ src/app/components/switch-theme/switch-theme.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-switch-theme',
+  imports: [],
+  templateUrl: './switch-theme.component.html',
+  styleUrl: './switch-theme.component.scss'
+})
+export class SwitchThemeComponent {
+
+}
