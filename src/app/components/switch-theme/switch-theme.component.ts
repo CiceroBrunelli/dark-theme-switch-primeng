1e5b06e9861d6317e9535cd21536b31594eb7744
Cicero Brunelli <cicerobrunelli@hotmail.com>
2025-01-11 12:41:31 -0300

    adiciona lógica de alternância de modo escuro ao componente SwitchTheme

--- src/app/components/switch-theme/switch-theme.component.ts
+++ src/app/components/switch-theme/switch-theme.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { InputSwitchModule } from 'primeng/inputswitch'; // Import the InputSwitchModule 
+import { FormsModule } from '@angular/forms'; // Import the FormsModule
+import { ThemeService } from '../../services/theme.service'; // Import the ThemeService
 
 @Component({
   selector: 'app-switch-theme',
-  imports: [],
+  imports: [InputSwitchModule, FormsModule], // Add the InputSwitchModule and FormsModule to the imports array
   templateUrl: './switch-theme.component.html',
   styleUrl: './switch-theme.component.scss'
 })
-export class SwitchThemeComponent {
+export class SwitchThemeComponent implements OnInit {
+
+  isDarkMode: boolean = false;
+
+  constructor(private themeService: ThemeService) { }
+
+  ngOnInit() {
+    // Inicializa o estado do switch com base no tema atual
+    const currentTheme = localStorage.getItem('app-theme');
+    this.isDarkMode = currentTheme === 'dark';
+  }
+
+  toggleDarkMode() {
+    this.isDarkMode = this.themeService.toggleDarkMode();
+  }
 
 }
