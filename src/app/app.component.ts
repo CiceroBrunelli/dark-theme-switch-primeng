3972e6d8c23d872a85da32b56af55252ab3dd540
Cicero Brunelli <cicerobrunelli@hotmail.com>
2025-01-11 13:03:33 -0300

    adiciona componente SwitchTheme ao módulo principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SwitchThemeComponent } from "./components/switch-theme/switch-theme.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SwitchThemeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
