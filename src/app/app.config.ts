486565ada4770fa7359d689930c9a25a14b24a41
Cicero Brunelli <cicerobrunelli@hotmail.com>
2025-01-11 12:19:39 -0300

    adiciona configuração do PrimeNG e tema Aura ao app

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,20 @@
+
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { providePrimeNG } from 'primeng/config'; // Import the providePrimeNG function
+import Aura from '@primeng/themes/aura'; // Import the Aura theme
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }),
+  provideRouter(routes),
+  providePrimeNG({
+    theme: {
+      preset: Aura,
+      options: {
+        darkModeSelector: '.my-app-dark'
+      }
+    },
+  }),
+  ]
 };
